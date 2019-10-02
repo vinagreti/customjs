1e8df79ef3e95b8ef841e994e11679837babf686
Bruno Joao <bruno@tzadi.com>
2019-10-01 21:29:30 -0300

    card

--- projects/customjs/select/src/public-api.ts
+++ projects/customjs/select/src/public-api.ts
@@ -3,6 +3,10 @@
  */
 
 export * from './lib/custom-select-option/custom-select-option.component';
+export * from './lib/custom-select-option/custom-select-option.module';
 export * from './lib/custom-select.component';
 export * from './lib/custom-select.module';
+export * from './lib/custom-single-select/custom-single-select.component';
+export * from './lib/custom-single-select/custom-single-select.module';
+export * from './lib/custom-single-select/testing';
 export * from './lib/testing/custom-select.testing.module';
